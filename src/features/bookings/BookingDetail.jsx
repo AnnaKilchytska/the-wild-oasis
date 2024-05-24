1398757bd30ac11f1f3e75c8ba4ef1d8f4e00812
Anna <80330820+AnnaKilchytska@users.noreply.github.com>
2024-05-24 17:25:16 +0200

    add error boundary and fix some bugs

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -17,6 +17,7 @@ import { useCheckout } from "../check-in-out/useCheckout";
 import { useDeleteBooking } from "./useDeleteBooking";
 import Modal from "../../ui/Modal";
 import ConfirmDelete from "../../ui/ConfirmDelete";
+import Empty from "../../ui/Empty";
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -34,10 +35,11 @@ function BookingDetail() {
   const moveBack = useMoveBack();
 
   if (isLoading) return <Spinner />;
+  if (!booking) return <Empty resourceName="booking" />;
 
   const { status, id: bookingId } = booking;
 
-  console.log(booking);
+  // console.log(booking);
 
   const statusToTagName = {
     unconfirmed: "blue",
--- src/ui/Menus.jsx
+++ src/ui/Menus.jsx
@@ -86,6 +86,7 @@ function Toggle({ id }) {
   const { openID, close, open, setPosition } = useContext(MenusContext);
 
   function handleClick(e) {
+    e.stopPropagation();
     const rect = e.target.closest("button").getBoundingClientRect();
     console.log(rect);
     setPosition({
@@ -105,7 +106,7 @@ function Toggle({ id }) {
 function List({ id, children }) {
   const { openID, position, close } = useContext(MenusContext);
 
-  const ref = useOutsideClick(close);
+  const ref = useOutsideClick(close, false);
 
   if (openID !== id) return null;
 
