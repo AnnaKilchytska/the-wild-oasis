2fa86fe53b3a80e603d03c5f781a61ef8571c3fb
Anna <80330820+AnnaKilchytska@users.noreply.github.com>
2024-05-25 12:59:32 +0200

    clean up

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import SpinnerMini from "../../ui/SpinnerMini";
 import { useLogin } from "./useLogin";
 
 function LoginForm() {
-  const [email, setEmail] = useState("anna@example.com");
-  const [password, setPassword] = useState("anna123456");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
